4b26afa321b2c077b83439be71ba33781be5c6ba
Pablo Pinargote <pablo.pinargote@outlook.com>
2021-08-17 13:22:53 -0500

    Dynamic file choices support

--- djangocms_cssimport/models.py
+++ djangocms_cssimport/models.py
@@ -3,19 +3,13 @@ from cms.models import CMSPlugin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
+from django.utils.functional import lazy
 
 
 class CSSImportSpec(CMSPlugin):
+
     REPOSITORY_CHOICES = tuple((entry.replace(settings.BASE_DIR, ''),
                                 entry.replace(settings.BASE_DIR, '')) for entry in settings.STATICFILES_DIRS)
-    FILE_CHOICES = []
-    for i, repos in enumerate(settings.STATICFILES_DIRS):
-        for r, d, f in os.walk(repos):
-            for file in f:
-                if not file.endswith(".css"):
-                    continue
-                entry = os.path.join(r, file).replace(settings.BASE_DIR, '').replace(f'{REPOSITORY_CHOICES[i][0]}/', '')
-                FILE_CHOICES.append(tuple((entry, entry)))
 
     repository = models.CharField(
         verbose_name=_('Repository'),
@@ -28,7 +22,7 @@ class CSSImportSpec(CMSPlugin):
 
     css_file_path = models.CharField(
         verbose_name=_('CSS File Path'),
-        choices=FILE_CHOICES,
+        choices=(),
         blank=False,
         default='',
         null=True,
@@ -37,4 +31,18 @@ class CSSImportSpec(CMSPlugin):
     )
 
     def __str__(self):
+        self._meta.get_field('css_file_path').choices = lazy(self.get_file_choices, list)()
         return self.css_file_path.split('/')[len(self.css_file_path.split('/')) - 1] or str(self.pk)
+
+    def get_file_choices(self):
+        file_choices = []
+        for i, repos in enumerate(settings.STATICFILES_DIRS):
+            for r, d, f in os.walk(repos):
+                for file in f:
+                    if not file.endswith(".css"):
+                        continue
+                    entry = os.path.join(r, file)
+                    entry = entry.replace(settings.BASE_DIR, '')
+                    entry = entry.replace(f'{self.REPOSITORY_CHOICES[i][0]}/', '')
+                    file_choices.append(tuple((entry, entry)))
+        return file_choices
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ REQUIREMENTS = [
 
 setuptools.setup(
     name="djangocms_cssimport",
-    version="1.0.5",
+    version="1.0.6",
     author="Pablo Pinargote",
     author_email="pablo.pinargote@outlook.com",
     description="Plugin for django CMS that allows you to add internal CSS files on demand.",
